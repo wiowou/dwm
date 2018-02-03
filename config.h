b14940bd820d7fcbc7a93ddc832cfb0351058a9a
bk <wiowou@hotmail.com>
2018-02-03 10:50:05 -0500

    no special rules for eclipse

--- config.h
+++ config.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
-	{ "Eclipse",  NULL,       NULL,       0,            1,           -1 },
+//	{ "Eclipse",  NULL,       NULL,       0,            1,           -1 },
 	{ "Feh",      NULL,       NULL,       0,            1,           -1 },
 };
 
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "urxvt", NULL };
+static const char *termcmd[]  = { "urxvt-dir", NULL };
 
 #include "push.c"
 
